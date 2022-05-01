ca181fffddd4a57c804c1de632c58aef3a7a4ea8
Mayeul DE LA SERRE <mde-la-s@e1r9p20.clusters.42paris.fr>
2022-05-01 19:11:58 +0200

    cpp06

--- cpp06/incs/Conv.hpp
+++ cpp06/incs/Conv.hpp
@@ -2,6 +2,7 @@
 # define CONV_H
 
 #include <iostream>
+#include <cstdlib>
 
 class Conv {
 
@@ -12,7 +13,28 @@ public :
 	~Conv( void );
 	Conv & operator=( const Conv & rhs );
 
-private :
+	void analyseType( const char* value );
+	void getValues( const char* value );
+	void print( void );
+
+	int type;
+	static const int typeChar = 0;
+	static const int typeInt = 1;
+	static const int typeFloat = 2;
+	static const int typeDouble = 3;
+
+	char charValue;
+	int intValue;
+	float floatValue;
+	double doubleValue;
+
+class unknownTypeException : public std::exception {
+
+	public :
+
+		virtual const char* what() const throw() {return ("Cannot find a type for your entry");}
+
+	};
 
 };
 
