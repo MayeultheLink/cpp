0877afad9377d41bb35f206737970133e5398f37
Mayeul DE LA SERRE <mde-la-s@e3r3p7.clusters.42paris.fr>
2022-05-05 16:39:54 +0200

    ex02

---
+++ cpp06/ex02/incs/Base.hpp
@@ -0,0 +1,21 @@
+#ifndef BASE_HPP
+# define BASE_HPP
+
+#include <cstdlib>
+#include <time.h>
+
+class Base {
+
+	public :
+	
+	virtual ~Base( void );
+
+};
+
+class A : public Base {};
+
+class B : public Base {};
+
+class C : public Base {};
+
+#endif
