2a4c820faa61d448a67298737a95aec590380343
Mayeul DE LA SERRE <mde-la-s@e3r3p7.clusters.42paris.fr>
2022-05-05 18:53:34 +0200

    ex00

---
+++ cpp07/ex00/includes.hpp
@@ -0,0 +1,12 @@
+#ifndef INCLUDES_H
+# define INCLUDES_H
+
+
+template <typename T>
+T max(T x, T y);
+template <typename T>
+T min(T x, T y);
+template <typename T>
+void swap(T &x, T &y);
+
+#endif
