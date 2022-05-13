bc19ca4b82189dfbc43ef9312dc6b8fd9b55ab5f
Mayeul DE LA SERRE <mde-la-s@e1r3p6.42.fr>
2022-05-13 15:58:56 +0200

    ex02

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+
+	public :
+
+		MutantStack( void ) {}
+		MutantStack( const MutantStack & src ) { *this = src; }
+		~MutantStack( void ) {}
+		MutantStack & operator=( const MutantStack & ) { return *this; }
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin( void ) { return this->c.begin(); }
+		iterator end( void ) { return this->c.end(); }
+
+};
+
+#endif
